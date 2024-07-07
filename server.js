de433ecd6cd91d5274b2b7d02dfc9b9780d0c5b2
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-07-07 11:53:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
 
 
 // Routes
