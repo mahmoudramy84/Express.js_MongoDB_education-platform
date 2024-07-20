44f0919f0607cfbe3e22f5848c55006b10c1e666
mahmoudramy84 <mahmoudramy84@gmail.com>
2024-07-20 11:42:29 +0300

    Update cors port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5000',
     optionsSuccessStatus: 200
 };
 
